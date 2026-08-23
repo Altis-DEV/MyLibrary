16e2f58ee094e480e96f17233708d2f5f390f5ea
Altis <flandrescripter@gmail.com>
2026-08-23 17:44:41 +0700

    Add Theme.lua with color definitions

---
+++ Src/Theme.lua
@@ -0,0 +1,15 @@
+-- Path: Altis-DEV/MyLibrary/Src/Theme.lua
+
+return {
+
+    Accent1 = Color3.fromRGB(80,80,255),
+
+    Accent2 = Color3.fromRGB(40,40,40),
+
+    Background = Color3.fromRGB(20,20,20),
+
+    Text = Color3.fromRGB(235,235,235),
+
+    Border = Color3.fromRGB(120,120,120)
+
+}
