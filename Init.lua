58e109755da57e24caf2274df275ee746ec9da31
Altis <flandrescripter@gmail.com>
2026-08-23 17:44:20 +0700

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -1,1 +1,27 @@
+-- Path: Altis-DEV/MyLibrary/Init.lua
 
+local Repo = "Altis-DEV/MyLibrary/"
+
+local function LoadModule(path)
+    return loadstring(
+        game:HttpGet(
+            "https://raw.githubusercontent.com/" ..
+            Repo ..
+            path
+        )
+    )()
+end
+
+
+local Library = {}
+
+Library.Theme = LoadModule("Src/Theme.lua")
+
+Library.CreateWindow = function(config)
+    local Window = LoadModule("Src/Window.lua")
+
+    return Window.new(config, Library.Theme)
+end
+
+
+return Library
