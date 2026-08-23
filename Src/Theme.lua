4cdd419eed2f1bb452d534dc573ac26c2e3d1cff
Altis <flandrescripter@gmail.com>
2026-08-23 22:49:26 +0700

    Update Theme.lua

--- Src/Theme.lua
+++ Src/Theme.lua
@@ -1,9 +1,12 @@
+-- Src/Theme.lua
 local Theme = {
     Accent1 = Color3.fromRGB(24, 112, 196),   -- Màu đặc trưng của ImGui
     Accent2 = Color3.fromRGB(15, 75, 135),    -- Màu ImGui tối hơn
     Background = Color3.fromRGB(15, 25, 45),  -- Màu nền xanh dương đậm
+    Background2 = Color3.fromRGB(25, 40, 70), -- Màu nền sáng hơn Background
     Border = Color3.fromRGB(65, 65, 65),      -- Viền mặc định
     Text = Color3.fromRGB(255, 255, 255),     -- Màu chữ mặc định
+    Text2 = Color3.fromRGB(180, 180, 180),    -- Màu chữ tối hơn Text
     Font = Enum.Font.Code                     -- Font Code
 }
 
