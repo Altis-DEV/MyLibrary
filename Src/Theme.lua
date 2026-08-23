06b0568ef7c15677272a902cfa9ad239b75aff65
Altis <flandrescripter@gmail.com>
2026-08-23 23:07:19 +0700

    Update Theme.lua

--- Src/Theme.lua
+++ Src/Theme.lua
@@ -2,6 +2,7 @@
 local Theme = {
     Accent1 = Color3.fromRGB(24, 112, 196),   -- Màu đặc trưng của ImGui
     Accent2 = Color3.fromRGB(15, 75, 135),    -- Màu ImGui tối hơn
+    Highlight = Color3.fromRGB(44, 132, 216), -- Màu Accent1 sáng hơn (dùng khi Hover, v.v.)
     Background = Color3.fromRGB(15, 25, 45),  -- Màu nền xanh dương đậm
     Background2 = Color3.fromRGB(25, 40, 70), -- Màu nền sáng hơn Background
     Border = Color3.fromRGB(65, 65, 65),      -- Viền mặc định
